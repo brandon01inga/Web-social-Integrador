13b04c0a70cf5d9ae2adad2faabc8ddeb9023fe0
MiltonSuarez <suarezvitealdair@gmail.com>
2025-06-05 16:59:26 -0500

    Eliiminando archivos y haciendo modelos de publicacion

--- Publicaciones/models.py
+++ Publicaciones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Usuarios.models import Usuario
 
 # Create your models here.
+
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=200)
+    imagen = models.ImageField(upload_to='imagenes_publicaciones/')
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
